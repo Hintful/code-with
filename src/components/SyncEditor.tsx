a7c94b517441556af4d9e6aea093fa864759576b
Kurt Choi <choi.kurt3@gmail.com>
2021-03-01 22:02:59 -0500

    basic functionality working - double emit leading to overflow needs fix

--- src/components/SyncEditor.tsx
+++ src/components/SyncEditor.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { createEditor } from 'slate';
+import { createEditor, Operation } from 'slate';
 import { Editable, Slate, withReact } from 'slate-react';
 import { InitStateObject } from './InitStateObject';
+import io from 'socket.io-client';
+
+const SERVER_PORT = 4000;
+const socket = io(`http://localhost:${SERVER_PORT}`);
 
 export interface SyncEditorProps {
   
@@ -10,11 +14,30 @@ export interface SyncEditorProps {
 const SyncEditor: React.SFC<SyncEditorProps> = () => {
   const editor = React.useMemo(() => withReact(createEditor()), []);
   const [value, setValue] = React.useState([InitStateObject]);
+  const [remoteChange, setRemoteChange] = React.useState(false);
+
+  React.useEffect(() => {
+    socket.on('new-remote-op', ({ ops }: { ops: string }) => {
+      console.log("x");
+      const parsed_ops = JSON.parse(ops);
+      setRemoteChange(true);
+      setValue(parsed_ops);
+    })
+  })
   return (  
     <Slate
       editor={editor}
       value={value}
-      onChange={newValue => setValue(newValue as any)}>
+      onChange={newValue => {
+        if (!remoteChange) {
+          socket.emit("new-op", {
+            ops: JSON.stringify(newValue)
+          });
+        }
+        setValue(newValue as any);
+        setRemoteChange(false);
+      }}
+    >
         <Editable />
     </Slate>
   );
