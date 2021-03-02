3a566cc9913f7beae7f7b6ec6cf7d015cac75834
Kurt Choi <choi.kurt3@gmail.com>
2021-03-01 20:01:16 -0500

    add initial editor skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useMemo, useState } from 'react';
+import { createEditor } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
 
 function App() {
+  const editor = useMemo(() => withReact(createEditor()), []);
+
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }]
+    }
+  ]);
   return (
     <div className="App">
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={newValue => setValue(newValue as any)}>
+          <Editable />
+      </Slate>
     </div>
   );
 }
